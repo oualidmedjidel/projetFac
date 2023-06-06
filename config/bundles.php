e6465a5e4b2e398daf55f4fb9147595f4eceb8dd
Oualid <medjidel.oualid@gmail.com>
2023-06-06 18:38:11 +0200

    creation de la branche login et ajout de la fonctionnalité login avec la commande php bin/console make:auth

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
